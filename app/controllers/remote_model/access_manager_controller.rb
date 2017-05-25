28d7bfea6d7c5b5cd6ad977a9e3503d046f8e8f0
Aashutosh Chaudhary <aashutosh@unnatihelpers.com>
2017-05-25 16:13:36 +0530

    exception handling

--- app/controllers/remote_model/access_manager_controller.rb
+++ app/controllers/remote_model/access_manager_controller.rb
@@ -5,8 +5,10 @@ module RemoteModel
       method_name = params[:method_name]
       arguments = params[:arguments]
       attribute_name = params[:attribute_name]
-      object = model_name.constantize.send method_name, arguments
-      attribute_value = object.send attribute_name
+      if model_name.present? and method_name.present? and arguments.present? and attribute_name.present?
+        object = model_name.constantize.send method_name, arguments rescue nil
+        attribute_value = object.send attribute_name if object.present?
+      end
       render :plain => attribute_value, :status => 200
     end
   end
--- lib/remote_model.rb
+++ lib/remote_model.rb
@@ -8,14 +8,17 @@ require "remote_model/config"
 module RemoteModel
   # Your code goes here...
   def self.access argument
-    entities = argument.split('.')
+    entities = Array.new
+    entities = argument.split('.') if argument.present?
     model_name = entities[0]
-    method_name = entities[1].split("\(").first
-    arguments = entities[1].split("\(").last.split("\)").first
+    method_name = entities[1].split("\(").first if entities[1].present?
+    arguments = entities[1].split("\(").last.split("\)").first if entities[1].present?
     attribute_name = entities[2]
-    
-    url = RemoteModel.remote_url + '/remote_model?model_name=' + model_name + '&method_name=' + method_name + '&arguments=' + arguments + '&attribute_name=' + attribute_name
-    response = HTTParty.get(url)
-    return response.body
+    if model_name.present? and method_name.present? and arguments.present? and attribute_name.present?
+      url = RemoteModel.remote_url + '/remote_model?model_name=' + model_name + '&method_name=' + method_name + '&arguments=' + arguments + '&attribute_name=' + attribute_name
+      response = HTTParty.get(url) rescue nil
+      response = response.body if response.present? and response.body.present?
+    end
+    return response
   end
 end
